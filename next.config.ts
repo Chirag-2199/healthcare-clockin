648c2c48cea164707b1c1c18b7035c0902d7f218
Chirag-2199 <chiragkumar2199@gmail.com>
2025-04-02 18:32:28 +0530

    fixed deployment issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  output: "export", // Enables static export
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js Image Optimization
   },
